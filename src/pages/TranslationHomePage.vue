dfe4e5904e8c6b4d1458ddc8cb6b6a8dae427810
adamhider <ajder.adjivapov@gmail.com>
2023-12-13 22:02:10 +0300

    Translation page improvements

--- src/pages/TranslationHomePage.vue
+++ src/pages/TranslationHomePage.vue
@@ -1,124 +1,115 @@
 <template>
   <q-page-wrapper fullscreen>
     <AppBackgroundBubles :intense="isFocused"/>
-    <q-app-header class="bg-transparent sticky" reveal>
-      <TranslationLanguageToggle :source_language_id="data.source_language_id" :target_language_id="data.target_language_id" @onChange="onLanguageChange"/>
+    <q-app-header class="bg-transparent sticky" reveal centered>
+      <TranslationLanguageToggle
+        transparent
+        :source_language_id="data.source_language_id"
+        :target_language_id="data.target_language_id"
+        @onChange="onLanguageChange"/>
     </q-app-header>
-    <q-page class="q-pa-sm row column  content-around justify-end" style="transition: all 2s ease">
-    <q-card v-if="isFocused && wordOptions.length>0" class="q-ma-sm">
-      <q-card-section class="full-width q-px-none">
-        <q-list >
-          <q-item v-ripple clickable v-for="(wordPrediction, wordPredictionIndex) in wordOptions" :key="wordPredictionIndex">
-            <q-item-section top avatar>
-              <q-avatar size="md">
-                <q-img :src="getLanguage(wordPrediction.language_id, 'id')?.flag"></q-img>
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label><b v-html="wordPrediction.wordMarked"></b></q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-card-section>
-    </q-card>
-    <q-card v-else flat class="bg-transparent no-border q-ma-sm">
-      <q-card-section align="center">
-        <div class="text-h5 text-white"><b>Explore the Dictionary</b></div>
-        <div class="text-caption text-white"><b>The everything and get the answer!</b></div>
-      </q-card-section>
-    </q-card>
-      <q-card flat :class="`full-width q-my-sm bg-transparent no-border ${(isFocused) ? 'flex-item-0' : 'flex-item-0-5'} transition-0-5s`">
-          <q-card-section class="full-width q-pa-sm">
-            <q-input
-              ref="searchInput"
-              v-model="data.token"
-              dark
-              standout="bg-white text-primary"
-              debounce="100"
-              placeholder="Enter a word..."
-              @keydown.enter.prevent="router.push(searchLink)"
-              @update:model-value="autocomplete"
-              class="no-box-shadow"
-              @focus="isFocused=true"
-              @blur="blurInput"
-            >
-              <template v-slot:append>
-                <q-icon v-if="data.token != ''" name="close" @click="data.token = ''; clearInput()" class="q-mr-sm cursor-pointer" />
-                <q-btn v-if="!isFocused" flat  icon="search" :to="searchLink"/>
-              </template>
-            </q-input>
+    <q-page class="q-pa-sm row content-center justify-center" style="transition: all 2s ease">
+      <transition appear  enter-active-class="animated zoomIn" leave-active-class="animated zoomOut">
+        <q-card  v-if="!isFocused" flat class="bg-transparent no-border q-ma-sm">
+          <q-card-section align="center">
+            <div class="text-h5 text-white"><b>Tap to search...</b></div>
+            <div class="text-caption text-white"><b>The everything and get the answer!</b></div>
           </q-card-section>
-      </q-card>
+          <q-card-section align="center">
+            <q-btn
+              class="search-button"
+              color="white"
+              flat
+              round
+              size="xl"
+              :icon="(isFocused) ? 'close' : 'search'"
+              :disable="isFocused"
+              style="background: #ffffff20;"
+              @click="isFocused = !isFocused"
+            />
+          </q-card-section>
+        </q-card>
+      </transition>
+      <q-dialog v-model="isFocused" position="bottom">
+        <TranslationSearchBar
+          transparent
+          :source_language_id="data.source_language_id"
+          :target_language_id="data.target_language_id"
+          :focused="isFocused"
+          @onSubmit="search"
+        />
+        </q-dialog>
     </q-page>
   </q-page-wrapper>
 </template>
 
 <script setup >
-import { api } from '../services/index'
 import { ref, reactive, watch, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import TranslationLanguageToggle from '../components/TranslationLanguageToggle.vue'
-import AppBackgroundStars from '../components/AppBackgroundStars.vue'
 import AppBackgroundBubles from '../components/AppBackgroundBubles.vue'
-import { useLanguage } from '../composables/useLanguage'
+import TranslationSearchBar from '../components/TranslationSearchBar.vue'
 
 const route = useRoute()
 const router = useRouter()
-const { getLanguage } = useLanguage()
 
 const isFocused = ref(false)
-const searchInput = ref(null)
 
 const data = reactive({
-  token: '',
-  source_language_id: "2",
-  target_language_id: "1"
+  source_language_id: "1",
+  target_language_id: "2"
 })
-const wordOptions = ref([])
+
+const search = function (val) {
+  router.push(`/translation/q=${val}&sl=${data.source_language_id}&tl=${data.target_language_id}`)
+}
+
+const onFocusChange = function(val){
+  isFocused.value = val
+  if(val) route.meta.bottomBarEnabled = false
+}
+
 const onLanguageChange = function(languages){
   data.source_language_id = languages.source_language_id
   data.target_language_id = languages.target_language_id
 }
 
-const searchLink = computed(() => {
-  return `/translation/${getLanguage(data.source_language_id, 'id')?.title}-${getLanguage(data.target_language_id, 'id')?.title}/${data.token}`
+watch(() => isFocused.value, (currentValue, oldValue) => {
+  if(currentValue) route.meta.bottomBarEnabled = false
+  console.log(route.meta)
 })
-
-const autocomplete = async function(val) {
-  if(val == '') return wordOptions.value = []
-  const wordListResponse = await api.word.autocomplete({ filter: { word: val }, limit: 7, offset: 0})
-  if (wordListResponse.error) {
-    wordOptions.value = []
-    return
-  }
-  for(var i in wordListResponse) {
-    wordListResponse[i].wordMarked = wordListResponse[i].word.replace(val, `<b style="font-weight: 400">${val}</b>`)
-  }
-  wordOptions.value = wordListResponse
-}
-
-const clearInput = function () {
-  wordOptions.value = []
-  searchInput.value.blur()
-}
-const blurInput = function () {
-  if(data.token == '') {
-    wordOptions.value = []
-    isFocused.value = false
-  }  else {
-    searchInput.value.focus()
-    return false
-  }
-}
-
 
 
 </script>
-<style>
 
-.tagged{
-  color: red;
+<style scoped>
+
+.search-button::before,
+.search-button::after {
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background: transparent;
+  border: 2px solid white;
+  opacity: 0.5;
+  transform: scale(1);
+  content: '';
+
+
+}
+.search-button::before{
+  animation: pulse 2s ease-out infinite;
+}
+.search-button::after {
+  animation: pulse 2s 1s ease-out infinite ;
 }
 
-
+@keyframes pulse {
+  100% {
+    transform: scale(1.5);
+    opacity: 0;
+  }
+}
 </style>
